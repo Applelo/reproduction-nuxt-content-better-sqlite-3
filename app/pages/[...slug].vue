e7cedfa704f29eb18b40b0ba98b1d8d48c044027
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2026-01-14 20:05:08 +0100

    add nuxt content example

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page, error, execute } = await useAsyncData('page-' + route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+</script>
+
+<template>
+  <div>
+    <ContentRenderer
+      v-if="page"
+      :value="page"
+    />
+    <div v-else-if="error">
+      <h2>Error</h2>
+      <p>Message: {{ error.message }}</p>
+      <p>Data: {{ error.data }}</p>
+      <p>Cause: { error.cause }}</p>
+      <button @click="execute()">Fetch on client side</button>
+    </div>
+  </div>
+</template>
